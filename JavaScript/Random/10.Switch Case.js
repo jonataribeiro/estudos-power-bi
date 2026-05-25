df4e96ebd5fe419ae03ed4bfe1ee8077e3802cd5
jonataribeiro <jonataribeiro.dev@gmail.com>
2026-05-25 02:33:20 -0300

    Conclusão do Módulo de JavaScript pt-2

---
+++ JavaScript/Random/10.Switch Case.js
@@ -0,0 +1,51 @@
+/*
+Controladores de Fluxo - Switch Case
+
+*/
+
+/*
+const temperature = 22
+
+switch (temperature) {
+    case 10:
+        console.log("Está muito frio")
+        break;
+
+    case 20:
+        console.log("Está meio quente")
+        break;
+
+    case 30:
+        console.log("Está muito quente")
+        break;
+
+
+    default:
+        console.log("Não sei te dizer")
+        break;
+}
+
+*/
+
+
+
+const user = "Jônata"
+
+switch (user) {
+    case "Jônata":
+        console.log("Está muito frio")
+        break;
+
+    case "Maria":
+        console.log("Está meio quente")
+        break;
+
+    case "Joaquim":
+        console.log("Está muito quente")
+        break;
+
+
+    default:
+        console.log("Não sei te dizer")
+        break;
+}
---
+++ JavaScript/Random/5.Operadores-comparação pt.2.js
@@ -0,0 +1,15 @@
+/*
+
+Operações de Comparação
+
+>   -> Maior
+<   -> Menor
+
+>=  -> Maior ou Igual
+<=  -> Menor ou Igual
+*/
+
+const firstNumber = 20
+const seccondNumber = 20
+
+console.log(firstNumber >= seccondNumber)
